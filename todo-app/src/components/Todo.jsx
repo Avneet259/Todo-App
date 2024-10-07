39357313eae9163ae24c445233920451461701b4
AVNEET KAUR <avibal1325@gmail.com>
2024-10-07 21:02:54 +0530

    Todo.jsx, TodoItems.jsx,TodoItems.css updated

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -11,10 +11,19 @@ const Todo = () => {
     const add = () => {
         setTodos([...todos, {no:count++, text:inputRef.current.value, display:""}])
         inputRef.current.value = ""
+        localStorage.setItem("todos-count", count)
     }
 
     useEffect(() => {
-        console.log(todos)
+        setTodos(JSON.parse(localStorage.getItem("todos")))
+        count = localStorage.getItem("todos-count")
+    }, [])
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(todos)
+            localStorage.setItem("todos", JSON.stringify(todos))
+        }, 100)
     }, [todos])
 
   return (
@@ -26,7 +35,7 @@ const Todo = () => {
         </div>
         <div className="todo-list">
             {todos.map((item, index) => {
-                return <TodoItems key={index} no={item.no} display={item.display} text={item.text} />
+                return <TodoItems key={index} no={item.no} display={item.display} text={item.text} setTodos={setTodos} />
             })}
         </div>
     </div>
